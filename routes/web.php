f3b1cf3193de1decf34b6df0c2a364cda21d7504
ilhamlutfi <ilhamlutfi153@gmail.com>
2026-05-13 15:18:00 +0700

    feat: setup routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Livewire\Dashboard\DashboardPage;
+use App\Livewire\Transactions\TransactionList;
+use App\Livewire\Archive\ArchiveList;
+use App\Livewire\Wallets\WalletList;
+use App\Livewire\SavingGoals\GoalList;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/',            DashboardPage::class)->name('dashboard');
+    Route::get('/transactions', TransactionList::class)->name('transactions');
+    Route::get('/archive',      ArchiveList::class)->name('archive');
+    Route::get('/wallets',      WalletList::class)->name('wallets');
+    Route::get('/goals',        GoalList::class)->name('goals');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__ . '/auth.php';
